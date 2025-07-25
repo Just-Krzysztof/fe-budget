096cf022d2277d3b773640cfa99c82021d1137bd
Krzysztof <krzysztofjustgdynia@gmail.com>
2025-07-25 23:31:48 +0200

    fixed styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <h1 className="underline">test</h1>
+      <div role="alert" className="border-s-4 border-red-700 bg-red-50 p-4">
+        <div className="flex items-center gap-2 text-red-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <strong className="font-medium"> Something went wrong </strong>
+        </div>
+
+        <p className="mt-2 text-sm text-red-700">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo quasi assumenda numquam
+          deserunt consectetur autem nihil quos debitis dolor culpa.
+        </p>
+      </div>
       <section className="bg-white lg:grid lg:h-screen lg:place-content-center">
         <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
           <div className="mx-auto max-w-prose text-center">
