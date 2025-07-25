4992ae5b4f5846fa93af037e950023a53c2a97d6
Krzysztof <krzysztofjustgdynia@gmail.com>
2025-07-25 23:47:39 +0200

    version rollback of tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react';
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <h1 className="underline">test</h1>
+    <main className="min-h-screen bg-gray-50 text-gray-900 font-sans p-8">
       <div role="alert" className="border-s-4 border-red-700 bg-red-50 p-4">
         <div className="flex items-center gap-2 text-red-700">
           <svg
@@ -29,83 +24,221 @@ function App() {
           deserunt consectetur autem nihil quos debitis dolor culpa.
         </p>
       </div>
-      <section className="bg-white lg:grid lg:h-screen lg:place-content-center">
-        <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
-          <div className="mx-auto max-w-prose text-center">
-            <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
-              Understand user flow and
-              <strong className="text-indigo-600"> increase </strong>
-              conversions
-            </h1>
+      <div className="flow-root">
+        <dl className="-my-3 divide-y divide-gray-200 text-sm *:even:bg-gray-50">
+          <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Title</dt>
 
-            <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, nisi. Natus,
-              provident accusamus impedit minima harum corporis iusto.
-            </p>
+            <dd className="text-gray-700 sm:col-span-2">Mr</dd>
+          </div>
 
-            <div className="mt-4 flex justify-center gap-4 sm:mt-6">
-              <a
-                className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
-                href="#"
+          <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Name</dt>
+
+            <dd className="text-gray-700 sm:col-span-2">John Frusciante</dd>
+          </div>
+
+          <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Occupation</dt>
+
+            <dd className="text-gray-700 sm:col-span-2">Guitarist</dd>
+          </div>
+
+          <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Salary</dt>
+
+            <dd className="text-gray-700 sm:col-span-2">$1,000,000+</dd>
+          </div>
+
+          <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Bio</dt>
+
+            <dd className="text-gray-700 sm:col-span-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et facilis debitis explicabo
+              doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+              aspernatur neque molestiae labore aliquam soluta architecto?
+            </dd>
+          </div>
+        </dl>
+      </div>
+      <label htmlFor="Email" className="relative">
+        <input
+          type="email"
+          id="Email"
+          placeholder=""
+          className="peer mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm"
+        />
+
+        <span className="absolute inset-y-0 start-3 -translate-y-5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-5">
+          Email
+        </span>
+      </label>
+      <div>
+        <h2 className="sr-only">Steps</h2>
+
+        <div>
+          <div className="overflow-hidden rounded-full bg-gray-200">
+            <div className="h-2 w-1/2 rounded-full bg-blue-500"></div>
+          </div>
+
+          <ol className="mt-4 grid grid-cols-3 text-sm font-medium text-gray-500">
+            <li className="flex items-center justify-start text-blue-600 sm:gap-1.5">
+              <span className="hidden sm:inline"> Details </span>
+
+              <svg
+                className="size-6 sm:size-5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
               >
-                Get Started
-              </a>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
+                />
+              </svg>
+            </li>
 
-              <a
-                className="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900"
-                href="#"
+            <li className="flex items-center justify-center text-blue-600 sm:gap-1.5">
+              <span className="hidden sm:inline"> Address </span>
+
+              <svg
+                className="size-6 sm:size-5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
               >
-                Learn More
-              </a>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+            </li>
+
+            <li className="flex items-center justify-end sm:gap-1.5">
+              <span className="hidden sm:inline"> Payment </span>
+
+              <svg
+                className="size-6 sm:size-5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+                />
+              </svg>
+            </li>
+          </ol>
+        </div>
+      </div>
+      <header className="bg-white">
+        <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+          <a className="block text-teal-600" href="#">
+            <span className="sr-only">Home</span>
+            <svg className="h-8" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0.41 10.3847C1.14777 7.4194 2.85643 4.7861 5.2639 2.90424C7.6714 1.02234 10.6393 0 13.695 0C16.7507 0 19.7186 1.02234 22.1261 2.90424C24.5336 4.7861 26.2422 7.4194 26.98 10.3847H25.78C23.7557 10.3549 21.7729 10.9599 20.11 12.1147C20.014 12.1842 19.9138 12.2477 19.81 12.3047H19.67C19.5662 12.2477 19.466 12.1842 19.37 12.1147C17.6924 10.9866 15.7166 10.3841 13.695 10.3841C11.6734 10.3841 9.6976 10.9866 8.02 12.1147C7.924 12.1842 7.8238 12.2477 7.72 12.3047H7.58C7.4762 12.2477 7.376 12.1842 7.28 12.1147C5.6171 10.9599 3.6343 10.3549 1.61 10.3847H0.41ZM23.62 16.6547C24.236 16.175 24.9995 15.924 25.78 15.9447H27.39V12.7347H25.78C24.4052 12.7181 23.0619 13.146 21.95 13.9547C21.3243 14.416 20.5674 14.6649 19.79 14.6649C19.0126 14.6649 18.2557 14.416 17.63 13.9547C16.4899 13.1611 15.1341 12.7356 13.745 12.7356C12.3559 12.7356 11.0001 13.1611 9.86 13.9547C9.2343 14.416 8.4774 14.6649 7.7 14.6649C6.9226 14.6649 6.1657 14.416 5.54 13.9547C4.4144 13.1356 3.0518 12.7072 1.66 12.7347H0V15.9447H1.61C2.39051 15.924 3.154 16.175 3.77 16.6547C4.908 17.4489 6.2623 17.8747 7.65 17.8747C9.0377 17.8747 10.392 17.4489 11.53 16.6547C12.1468 16.1765 12.9097 15.9257 13.69 15.9447C14.4708 15.9223 15.2348 16.1735 15.85 16.6547C16.9901 17.4484 18.3459 17.8738 19.735 17.8738C21.1241 17.8738 22.4799 17.4484 23.62 16.6547ZM23.62 22.3947C24.236 21.915 24.9995 21.664 25.78 21.6847H27.39V18.4747H25.78C24.4052 18.4581 23.0619 18.886 21.95 19.6947C21.3243 20.156 20.5674 20.4049 19.79 20.4049C19.0126 20.4049 18.2557 20.156 17.63 19.6947C16.4899 18.9011 15.1341 18.4757 13.745 18.4757C12.3559 18.4757 11.0001 18.9011 9.86 19.6947C9.2343 20.156 8.4774 20.4049 7.7 20.4049C6.9226 20.4049 6.1657 20.156 5.54 19.6947C4.4144 18.8757 3.0518 18.4472 1.66 18.4747H0V21.6847H1.61C2.39051 21.664 3.154 21.915 3.77 22.3947C4.908 23.1889 6.2623 23.6147 7.65 23.6147C9.0377 23.6147 10.392 23.1889 11.53 22.3947C12.1468 21.9165 12.9097 21.6657 13.69 21.6847C14.4708 21.6623 15.2348 21.9135 15.85 22.3947C16.9901 23.1884 18.3459 23.6138 19.735 23.6138C21.1241 23.6138 22.4799 23.1884 23.62 22.3947Z"
+                fill="currentColor"
+              />
+            </svg>
+          </a>
+
+          <div className="flex flex-1 items-center justify-end md:justify-between">
+            <nav aria-label="Global" className="hidden md:block">
+              <ul className="flex items-center gap-6 text-sm">
+                <li>
+                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#">
+                    {' '}
+                    About{' '}
+                  </a>
+                </li>
+
+                <li>
+                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#">
+                    {' '}
+                    Careers{' '}
+                  </a>
+                </li>
+
+                <li>
+                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#">
+                    {' '}
+                    History{' '}
+                  </a>
+                </li>
+
+                <li>
+                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#">
+                    {' '}
+                    Services{' '}
+                  </a>
+                </li>
+
+                <li>
+                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#">
+                    {' '}
+                    Projects{' '}
+                  </a>
+                </li>
+
+                <li>
+                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#">
+                    {' '}
+                    Blog{' '}
+                  </a>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="flex items-center gap-4">
+              <div className="sm:flex sm:gap-4">
+                <a
+                  className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
+                  href="#"
+                >
+                  Login
+                </a>
+
+                <a
+                  className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
+                  href="#"
+                >
+                  Register
+                </a>
+              </div>
+
+              <button className="block rounded-sm bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
+                <span className="sr-only">Toggle menu</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
-      </section>
-      <div role="alert" className="rounded-md border border-gray-300 bg-white p-4 shadow-sm">
-        <div className="flex items-start gap-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6 text-green-600"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-
-          <div className="flex-1">
-            <strong className="font-medium text-gray-900"> Changes saved </strong>
-
-            <p className="mt-0.5 text-sm text-gray-700">Your product changes have been saved.</p>
-          </div>
-
-          <button
-            className="-m-3 rounded-full p-1.5 text-gray-500 transition-colors hover:bg-gray-50 hover:text-gray-700"
-            type="button"
-            aria-label="Dismiss alert"
-          >
-            <span className="sr-only">Dismiss popup</span>
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-5"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-      </div>
-    </>
+      </header>
+    </main>
   );
 }
-
-export default App;
